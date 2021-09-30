2d816cf586be2704c1ecbf856e84a84e0c5d1f67
henryhiraki22 <henry.hiraki@gmail.com>
2021-09-29 23:39:26 -0300

    remove user_data

--- main.tf
+++ main.tf
@@ -17,13 +17,6 @@ resource "aws_instance" "app_server" {
     ami = "ami-054a31f1b3bf90920"
     instance_type = "t2.micro"
     key_name = "ec2-saopaulo"
-    user_data = <<-EOF
-    #!/bin/bash
-    cd /home/ubuntu
-    echo "<h1>Feito com terraform</h1>" > index.html
-    nohup busybox httpd -f -p 8080 &
-    EOF
-
     tags = {
         Name = "Teste aws"
     }
