3ec0d28d8f2fab1d27716070728f21a631bd27ef
henryhiraki22 <henry.hiraki@gmail.com>
2021-09-29 23:07:39 -0300

    modify main.tf

--- main.tf
+++ main.tf
@@ -13,17 +13,18 @@ provider "aws" {
     region = "sa-east-1"
 }
 
-resource "aws_security_group" "ssh" {
-    name = "ssh"
-    vpc_id = "vpc-e6ab6a80"
-}
-
 resource "aws_instance" "app_server" {
     ami = "ami-054a31f1b3bf90920"
     instance_type = "t2.micro"
     key_name = "ec2-saopaulo"
+    user_data = <<-EOF
+    #!/bin/bash
+    cd /home/ubuntu
+    echo "<h1>Feito com terraform</h1>" > index.html
+    nohup busybox httpd -f -p 8080 &
+    EOF
 
     tags = {
-        Name = "Primeira instancia"
+        Name = "Teste aws"
     }
 }
